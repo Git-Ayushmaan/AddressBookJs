8867d976d7ed3f72fad11a49c43dfdcf559d535d
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-03-03 20:07:03 +0530

    [Ayushmaan Tiwari]Add.Function to delete contact

--- src/addressBook.js
+++ src/addressBook.js
@@ -1,4 +1,3 @@
-
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phone, email) {
       this.firstName = firstName;
@@ -53,4 +52,13 @@ class Contact {
       addressBook[index] = newContact;
     }
   }
-  
+  function deleteContactByName(name) {
+    const index = addressBook.findIndex(
+      (contact) =>
+        contact.firstName === name.split(" ")[0] &&
+        contact.lastName === name.split(" ")[1]
+    );
+    if (index !== -1) {
+      addressBook.splice(index, 1);
+    }
+  }
